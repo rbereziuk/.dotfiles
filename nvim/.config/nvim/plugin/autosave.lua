f1ee6984c9ae76cfd0d9af79803a7d62d58baadd
Roman Bereziuk <roma.physicist@gmail.com>
2023-01-25 10:43:46 +0200

    Improve auto run file on save script in neovim

--- nvim/.config/nvim/plugin/autosave.lua
+++ nvim/.config/nvim/plugin/autosave.lua
@@ -1,15 +1,20 @@
+local api = vim.api
+
 local attach_to_buffer = function (output_bufnr, pattern, command)
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    group = vim.api.nvim_create_augroup("Save and run file", { clear = true }),
+  api.nvim_create_autocmd("BufWritePost", {
+    group = api.nvim_create_augroup("Save and run file", { clear = true }),
     pattern = pattern,
     callback = function ()
       local append_data = function (_, data)
         if data then
-          vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+          api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
         end
       end
 
-      vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, false, {"output of: main.go"})
+      local label_str = vim.fn.expand('%')
+      local res_str = "Output of: " .. label_str
+
+      api.nvim_buf_set_lines(output_bufnr, 0, -1, false, { res_str })
       vim.fn.jobstart(command, {
         stdout_buffered = true,
         on_stdout = append_data,
@@ -20,9 +25,10 @@ local attach_to_buffer = function (output_bufnr, pattern, command)
 end
 
 
-vim.api.nvim_create_user_command("AutoRunFileOnSave", function ()
-  local bufrn = vim.fn.input("Bufnr: ")
+api.nvim_create_user_command("AutoRunFileOnSave", function ()
+  --local bufrn = vim.fn.input("Bufnr: ")
+  local bufnr = vim.fn.bufnr('%')
   local pattern = vim.fn.input("Pattern: ")
   local command = vim.split(vim.fn.input("Command: "), " ")
-  attach_to_buffer(tonumber(bufrn), pattern, command)
+  attach_to_buffer(tonumber(bufnr), pattern, command)
 end, {})
