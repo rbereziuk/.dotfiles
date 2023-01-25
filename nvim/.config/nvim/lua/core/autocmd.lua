4384f0f36aeffb27fc6357f0e9617870d01dfa03
Roman Bereziuk <roma.physicist@gmail.com>
2023-01-25 10:42:47 +0200

    Fix opening help files in new tab in neovim

--- nvim/.config/nvim/lua/core/autocmd.lua
+++ nvim/.config/nvim/lua/core/autocmd.lua
@@ -5,5 +5,10 @@ local help_in_tab_group = api.nvim_create_augroup('HelpInTabs', { clear = true})
 api.nvim_create_autocmd('BufEnter', {
   group = help_in_tab_group,
   pattern = '*.txt',
-  callback = function() api.nvim_command('execute "normal \\<C-W>T"') end
+  callback = function()
+    local buftype = api.nvim_get_option_value('buftype', { scope = 'local'} )
+    if buftype == 'help' then
+      api.nvim_command('execute "normal \\<C-W>T"')
+    end
+  end
 })
