08c8d13d2aa0fac0844da03cb4c2124037e0e3b2
Roman Bereziuk <roma.physicist@gmail.com>
2023-04-13 23:19:42 +0300

    Move from packer to lazy

--- nvim/.config/nvim/lua/core/plugin_configs/init.lua
+++ nvim/.config/nvim/lua/core/plugin_configs/init.lua
@@ -10,14 +10,14 @@ require('core.plugin_configs.lsp')
 require('core.plugin_configs.complition')
 require('core.plugin_configs.lspsaga')
 
-require('core.plugin_configs.colorizer')
+-- require('core.plugin_configs.colorizer')
 
 require('core.plugin_configs.null-ls')
-require('core.plugin_configs.prettier')
+-- require('core.plugin_configs.prettier')
 
 require('core.plugin_configs.luasnip')
 require('core.plugin_configs.git')
-require('core.plugin_configs.terminal')
+-- require('core.plugin_configs.terminal')
 
 require('core.plugin_configs.others')
 
--- nvim/.config/nvim/lua/core/plugins.lua
+++ nvim/.config/nvim/lua/core/plugins.lua
@@ -1,123 +1,103 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
+require("lazy").setup({
+  'mhinz/vim-startify',
+  'folke/tokyonight.nvim',
+  'rose-pine/neovim',
+  'rebelot/kanagawa.nvim',
 
-return require('packer').startup({function(use)
-  use 'wbthomason/packer.nvim'
-  use 'mhinz/vim-startify'
-
-  use 'folke/tokyonight.nvim'
-  use 'rose-pine/neovim'
-  use 'rebelot/kanagawa.nvim'
-
-  use {
+  {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    requires = {
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
     }
-  }
-
-  use {
+  },
+  {
+    'akinsho/bufferline.nvim', 
+    version = "v3.*", 
+    dependencies = 'nvim-tree/nvim-web-devicons'
+  },
+  {
     'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
+  },
 
-  use {
-    'akinsho/bufferline.nvim',
-    tag = "v3.*",
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
-
-  use {
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-  use 'nvim-treesitter/nvim-treesitter'
+  {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+  'nvim-treesitter/nvim-treesitter-textobjects',
 
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  'lewis6991/gitsigns.nvim',
+  {
+    'windwp/nvim-autopairs',
+      config = function() require('nvim-autopairs').setup {} end
+  },
 
-  use {
-    'williamboman/mason.nvim',
-    run = ":MasonUpdate" -- :MasonUpdate updates registry contents
-  }
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+  },
+  "williamboman/mason-lspconfig.nvim",
 
-  use "williamboman/mason-lspconfig.nvim"
-
-  -- LSP
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-cmdline'
-  use 'hrsh7th/nvim-cmp'
-  use({'L3MON4D3/LuaSnip', tag = 'v1.*'})
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'onsails/lspkind.nvim'
-
-  use({
+  -- Configurations for LSP
+  'neovim/nvim-lspconfig', 
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+  'hrsh7th/nvim-cmp',
+  {
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "1.*",
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp"
+  },
+  'saadparwaiz1/cmp_luasnip',
+  'onsails/lspkind.nvim',
+  {
     "glepnir/lspsaga.nvim",
-    opt = true,
-    branch = "main",
     event = "LspAttach",
     config = function()
-      require("lspsaga").setup({})
+        require("lspsaga").setup({})
     end,
-    requires = {
+    dependencies = {
       {"nvim-tree/nvim-web-devicons"},
       --Please make sure you install markdown and markdown_inline parser
       {"nvim-treesitter/nvim-treesitter"}
     }
-  })
+  },
+  'ray-x/lsp_signature.nvim',
+  'jose-elias-alvarez/null-ls.nvim',
 
-  use {
-    'windwp/nvim-autopairs',
-      config = function() require('nvim-autopairs').setup {} end
-  }
-  use 'windwp/nvim-ts-autotag'
-
-
-  use 'NvChad/nvim-colorizer.lua'
-
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'MunifTanjim/prettier.nvim'
-
-  use {
-    'lewis6991/gitsigns.nvim',
-  }
-
-  use 'tpope/vim-surround'
-
-  use 'christoomey/vim-tmux-navigator'
-
-  use({ "asiryk/auto-hlsearch.nvim", tag = "1.0.0" })
-
-  use 'ray-x/lsp_signature.nvim'
-
-  use {
+  -- Other
+  'tpope/vim-surround',
+  'christoomey/vim-tmux-navigator',
+  {
     'numToStr/Comment.nvim',
     config = function()
         require('Comment').setup()
     end
-  }
-
-  use {'akinsho/toggleterm.nvim', tag = '*', config = function()
-    require("toggleterm").setup()
-  end}
-
-  use {
+  },
+  {
     'folke/zen-mode.nvim',
     config = function()
       require("zen-mode").setup {
@@ -126,21 +106,16 @@ return require('packer').startup({function(use)
         -- refer to the configuration section below
       }
     end
-  }
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
-end,
-config = {
-  display = {
-    open_fn = require('packer.util').float,
-    working_sym = ' ',
-    error_sym = ' ',
-    done_sym = ' ',
-    removed_sym = ' ',
-    moved_sym = ' ',
-  }
-}
+  },
+  { "asiryk/auto-hlsearch.nvim", tag = "1.0.0" },
+  -- 'windwp/nvim-ts-autotag'
+  -- 'NvChad/nvim-colorizer.lua'
+  -- 'MunifTanjim/prettier.nvim'
+  -- {
+  --   'akinsho/toggleterm.nvim', 
+  --   tag = '*', 
+  --   config = function()
+  --     require("toggleterm").setup()
+  --   end
+  -- }
 })
