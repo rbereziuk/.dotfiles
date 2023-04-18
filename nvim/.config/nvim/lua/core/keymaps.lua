0a0ace3ae34db756775dba6bb14d73be2044c97b
Roman Bereziuk <roma.physicist@gmail.com>
2023-04-18 21:56:35 +0300

    Imptove save file keymap

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,19 +1,18 @@
 local keymap = vim.keymap
 
---vim.g.mapleader = ','
 vim.g.mapleader = ' '
 
 keymap.set('i', 'df', '<Esc>')
-keymap.set('n', '<leader>w', '<cmd>w<CR>', { noremap = true, silent = true })
-keymap.set('n', '<C-s>', ':w<CR>')
+
+-- Save file
+--keymap.set('n', '<leader>w', '<cmd>w<CR>', { noremap = true, silent = true, desc = 'Save' })
+keymap.set({ 'n', 'i', 'v', 's' }, '<C-s>', '<cmd>w<CR><esc>', { noremap = true, silent = true, desc = 'Save file' })
 
 keymap.set('n', '<C-h>', '<C-w>h')
 keymap.set('n', '<C-j>', '<C-w>j')
 keymap.set('n', '<C-k>', '<C-w>k')
 keymap.set('n', '<C-l>', '<C-w>l')
 
--- Keys from Prime ---
----------------------
 -- Move selected text
 keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
