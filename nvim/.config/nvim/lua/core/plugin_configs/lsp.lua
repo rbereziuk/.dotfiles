349ec22a519297785bc17d48f94810eda5b6606c
Roman Bereziuk <roma.physicist@gmail.com>
2023-06-20 01:27:22 +0300

    Update lspsaga keymaps from doc

--- nvim/.config/nvim/lua/core/plugin_configs/lsp.lua
+++ nvim/.config/nvim/lua/core/plugin_configs/lsp.lua
@@ -24,8 +24,8 @@ lspconfig.lua_ls.setup{
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+--vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+--vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
@@ -65,4 +65,3 @@ for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
