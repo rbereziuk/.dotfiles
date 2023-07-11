c11f414df51286a7a0b76246feeab761ce870cd5
Roman Bereziuk <roma.physicist@gmail.com>
2023-07-11 11:18:00 +0300

    Add eslint setting to work only when config file present

--- nvim/.config/nvim/lua/core/plugin_configs/null-ls.lua
+++ nvim/.config/nvim/lua/core/plugin_configs/null-ls.lua
@@ -7,8 +7,12 @@ local async = event == 'BufWritePost'
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d,
-    null_ls.builtins.code_actions.eslint_d,
+    null_ls.builtins.diagnostics.eslint_d.with({
+       condition = function(utils)
+        return utils.root_has_file({ ".eslintrc.json" })
+      end,
+    }),
+    --null_ls.builtins.code_actions.eslint_d,
     null_ls.builtins.formatting.autopep8
   },
 
