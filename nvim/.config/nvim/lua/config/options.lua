d48b719b9f7fdf3f577fdd75cf6a5c5c7f33434f
Roman Bereziuk <roma.physicist@gmail.com>
2025-01-28 22:56:30 +0200

    neovim: add none-ls plugin

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -13,15 +13,16 @@ vim.opt.splitright = true
 vim.opt.ignorecase = true -- need for smartcase
 vim.opt.smartcase = true -- ignore case when all letters lower
 vim.opt.scrolloff = 5 -- padding if press z-enter
+--vim.opt.scrolloff = 999
 vim.opt.hidden = true -- allow change edited buffer
 vim.opt.mouse = 'a' -- enable all mouse actions
+vim.opt.termguicolors = true
 vim.opt.signcolumn = 'yes:1'
-
 --vim.opt.wrap = false
+
+-- Synchronizes the system clipboard
 --vim.opt.clipboard = 'unnamedplus'
 
 --vim.opt.virtualedit = "block"
 --vim.opt.inccommand = "split" --show preview of substitute
-vim.opt.termguicolors = true
 
---vim.opt.scrolloff = 999
