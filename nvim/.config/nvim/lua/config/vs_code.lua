1afc66e7962acd65fbfac6ffd437981b0bd656af
Roman Bereziuk <roma.physicist@gmail.com>
2026-06-18 00:25:11 +0300

    neovim: add vscode new keymaps

--- nvim/.config/nvim/lua/config/vs_code.lua
+++ nvim/.config/nvim/lua/config/vs_code.lua
@@ -1,5 +1,36 @@
 vim.g.mapleader = ' '
 
--- Mapping
-vim.keymap.set('n', '<leader>fe', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>')
-vim.keymap.set('n', '<leader>ff', '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
+--> # Mapping
+
+--> Toggle file explorer
+vim.keymap.set('n', '<leader>fe', function() 
+  require('vscode').call('workbench.action.toggleSidebarVisibility')
+end)
+--vim.keymap.set('n', '<leader>fe', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>')
+
+--> Find file
+vim.keymap.set('n', '<leader>ff', function() 
+  require('vscode').call('workbench.action.quickOpen')
+end)
+--vim.keymap.set('n', '<leader>ff', '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
+
+--> Next diagnostic
+vim.keymap.set('n', '<leader>]d', function()
+  require('vscode').call('editor.action.marker.next')
+end)
+
+--> Prev diagnostic
+vim.keymap.set('n', '<leader>[d', function()
+  require('vscode').call('editor.action.marker.prev')
+end)
+
+--> Trigger Code Actions
+vim.keymap.set('n', '<leader>ca', function()
+  require('vscode').call('closeMarkersNavigation')
+  require('vscode').call('editor.action.quickFix')
+end)
+
+--> Show hover (error details, type info, etc.)
+vim.keymap.set('n', '<leader>K', function()
+  require('vscode').call('editor.action.showHover')
+end)
