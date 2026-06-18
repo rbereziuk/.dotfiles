ba4c513b26082018da2f0389f600d1c68b699e4b
Roman Bereziuk <roma.physicist@gmail.com>
2026-06-19 00:42:07 +0300

    neovim: improve vscode keymaps

--- nvim/.config/nvim/lua/config/vs_code.lua
+++ nvim/.config/nvim/lua/config/vs_code.lua
@@ -5,33 +5,38 @@ local vscode = require('vscode')
 
 --> Toggle file explorer
 vim.keymap.set('n', '<leader>fe', function() 
-  vscode.call('workbench.action.toggleSidebarVisibility')
+  vscode.action('workbench.action.toggleSidebarVisibility')
 end)
---vim.keymap.set('n', '<leader>fe', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>')
 
 --> Find file
 vim.keymap.set('n', '<leader>ff', function() 
-  vscode.call('workbench.action.quickOpen')
+  vscode.action('workbench.action.quickOpen')
 end)
---vim.keymap.set('n', '<leader>ff', '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
+
+--> Find buffer / Tab switcher
+vim.keymap.set('n', '<leader>fb', function() 
+  vscode.action('workbench.action.showAllEditorsByMostRecentlyUsed')
+  --> same as above, but without search
+  -- vscode.action('workbench.action.quickOpenPreviousRecentlyUsedEditor')
+end)
 
 --> Next diagnostic
 vim.keymap.set('n', '<leader>]d', function()
-  vscode.call('editor.action.marker.next')
+  vscode.action('editor.action.marker.next')
 end)
 
 --> Prev diagnostic
 vim.keymap.set('n', '<leader>[d', function()
-  vscode.call('editor.action.marker.prev')
+  vscode.action('editor.action.marker.prev')
 end)
 
 --> Trigger Code Actions
 vim.keymap.set('n', '<leader>ca', function()
-  vscode.call('closeMarkersNavigation')
-  vscode.call('editor.action.quickFix')
+  vscode.action('closeMarkersNavigation')
+  vscode.action('editor.action.quickFix')
 end)
 
 --> Show hover (error details, type info, etc.)
 vim.keymap.set('n', '<leader>K', function()
-  vscode.call('editor.action.showHover')
+  vscode.action('editor.action.showHover')
 end)
