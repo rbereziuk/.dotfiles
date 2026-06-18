f46e38eebf2bc8bcbf01bcac82c003762eb09072
Roman Bereziuk <roma.physicist@gmail.com>
2026-06-18 21:58:43 +0300

    neovim: add project config setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,3 +3,15 @@ if vim.g.vscode then
 else
   require('config.lazy')
 end
+
+--> Project specific configs
+
+-- Get the name of the current working directory folder
+local current_project = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+-- Standardize the string for Luarocks/Neovim module naming
+-- (Replaces spaces/dashes with underscores to prevent syntax issues in require paths)
+local module_name = "project_configs." .. current_project:gsub("[%s%-]", "_")
+
+-- Safely try to require the project-specific config module
+pcall(require, module_name)
--- nvim/.config/nvim/lua/config/vs_code.lua
+++ nvim/.config/nvim/lua/config/vs_code.lua
@@ -1,36 +1,37 @@
 vim.g.mapleader = ' '
 
 --> # Mapping
+local vscode = require('vscode')
 
 --> Toggle file explorer
 vim.keymap.set('n', '<leader>fe', function() 
-  require('vscode').call('workbench.action.toggleSidebarVisibility')
+  vscode.call('workbench.action.toggleSidebarVisibility')
 end)
 --vim.keymap.set('n', '<leader>fe', '<Cmd>call VSCodeNotify("workbench.action.toggleSidebarVisibility")<CR>')
 
 --> Find file
 vim.keymap.set('n', '<leader>ff', function() 
-  require('vscode').call('workbench.action.quickOpen')
+  vscode.call('workbench.action.quickOpen')
 end)
 --vim.keymap.set('n', '<leader>ff', '<Cmd>call VSCodeNotify("workbench.action.quickOpen")<CR>')
 
 --> Next diagnostic
 vim.keymap.set('n', '<leader>]d', function()
-  require('vscode').call('editor.action.marker.next')
+  vscode.call('editor.action.marker.next')
 end)
 
 --> Prev diagnostic
 vim.keymap.set('n', '<leader>[d', function()
-  require('vscode').call('editor.action.marker.prev')
+  vscode.call('editor.action.marker.prev')
 end)
 
 --> Trigger Code Actions
 vim.keymap.set('n', '<leader>ca', function()
-  require('vscode').call('closeMarkersNavigation')
-  require('vscode').call('editor.action.quickFix')
+  vscode.call('closeMarkersNavigation')
+  vscode.call('editor.action.quickFix')
 end)
 
 --> Show hover (error details, type info, etc.)
 vim.keymap.set('n', '<leader>K', function()
-  require('vscode').call('editor.action.showHover')
+  vscode.call('editor.action.showHover')
 end)
