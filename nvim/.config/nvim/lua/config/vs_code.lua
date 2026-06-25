0d63f61d13e58e3b868354081a1e6e5cfa376a2e
Roman Bereziuk <roma.physicist@gmail.com>
2026-06-25 11:11:17 +0300

    neovim: fix keymap diagnoctics

--- nvim/.config/nvim/lua/config/vs_code.lua
+++ nvim/.config/nvim/lua/config/vs_code.lua
@@ -3,16 +3,16 @@ vim.g.mapleader = ' '
 --> # Mapping
 local vscode = require('vscode')
 
-vim.keymap.set('n', '<leader>fe', function() vscode.action('workbench.action.toggleSidebarVisibility') end, { desc = 'Toggle file explorer'})
-vim.keymap.set('n', '<leader>ff', function() vscode.action('workbench.action.quickOpen') end, { desc = 'Find file'})
+vim.keymap.set('n', '<leader>fe', function() vscode.action('workbench.action.toggleSidebarVisibility') end, { desc = 'Toggle file explorer' })
+vim.keymap.set('n', '<leader>ff', function() vscode.action('workbench.action.quickOpen') end, { desc = 'Find file' })
 vim.keymap.set('n', '<leader>fb', function() 
   vscode.action('workbench.action.showAllEditorsByMostRecentlyUsed')
   --> same as above, but without search
   -- vscode.action('workbench.action.quickOpenPreviousRecentlyUsedEditor')
-end, {desc = 'Find buffer / Tab switcher'})
+end, { desc = 'Find buffer / Tab switcher' })
 
-vim.keymap.set('n', '<leader>]d', function() vscode.action('editor.action.marker.next') end, { desc = 'Next diagnostic'})
-vim.keymap.set('n', '<leader>[d', function() vscode.action('editor.action.marker.prev') end, {desc = 'Prev diagnostic'})
+vim.keymap.set('n', ']d', function() vscode.action('editor.action.marker.next') end, { desc = 'Next diagnostic' })
+vim.keymap.set('n', '[d', function() vscode.action('editor.action.marker.prev') end, { desc = 'Prev diagnostic' })
 
 vim.keymap.set('n', '<leader>ca', function()
   vscode.action('closeMarkersNavigation')
